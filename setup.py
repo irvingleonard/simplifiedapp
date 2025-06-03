b9fa2d0a6a973c117981f46c750a72cebeab8e20
Irving Leonard <irvingleonard@gmail.com>
2025-06-03 17:45:24 -0400

    Updated build system

--- setup.py
+++ setup.py
@@ -5,35 +5,8 @@ ToDo:
 - Everything
 """
 
-import setuptools
+from setuptools import setup
 
 import simplifiedapp
 
-setuptools.setup(
-	url = 'https://github.com/irvingleonard/simplifiedapp',
-	author = 'Irving Leonard',
-	author_email = 'irvingleonard@gmail.com',
-	license='BSD 2-Clause "Simplified" License',
-	classifiers = [
-		'Development Status :: 4 - Beta',
-		'Environment :: Console',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: BSD License',
-		'Natural Language :: English',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3 :: Only',
-		'Topic :: Software Development :: Libraries',
-		'Topic :: Software Development :: Libraries :: Python Modules',
-	],
-	keywords = 'cli main setuptools',
-	install_requires = [
-		'docstring_parser',
-		'introspection',
-	],
-	python_requires = '>=3.9',
-	packages = setuptools.find_packages(),
-	
-	**simplifiedapp.object_metadata(simplifiedapp)
-)
+setup(**simplifiedapp.object_metadata(simplifiedapp))
